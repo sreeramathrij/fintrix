dbc20d347af9b13a51084e2e0903291c8fedb835
Sreeram Athrij <sreeramathrij344@gmail.com>
2025-06-08 12:04:48 +0530

    Tweaked the ui of the morePage

--- client/src/pages/more.tsx
+++ client/src/pages/more.tsx
@@ -20,8 +20,19 @@ import {
 } from "lucide-react";
 import NavFooter from "@/components/NavFooter";
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { useState } from "react";
 
 export default function MoreScreen() {
+  const user = {
+    icon: User,
+    name: "Alwin Albert",
+    email: "alwin@email.com",
+    phone: "+01 2345678910",
+    link: "/profile",
+    balance: 3756,
+    budget: 10000,
+  }
+
   const sections = [
     {
       name:"Customization",
@@ -44,15 +55,9 @@ export default function MoreScreen() {
     {
        name:"Info",
       items: [
-       
-        { icon: User, label: "Alwin Albert", link: "/profile" },
-      
-       
         { icon: Calendar, label: "Calendar", link: "/calendar" },
         { icon: AlarmClock, label: "Scheduled", link: "/scheduled" },
-       
         { icon: PiggyBank, label: "Goals", link: "/goals" },
-       
         {icon:Info,label:"About", subtitle:"Learn more about the app",link:"/about"},
       ]
     },
@@ -67,6 +72,10 @@ export default function MoreScreen() {
     }
   ];
 
+  const [theme, setTheme] = useState("dark");
+  const progress = (((user.budget - user.balance) / user.budget) * 100);
+  console.log(progress);
+
   return (
     
     
@@ -74,44 +83,43 @@ export default function MoreScreen() {
       {/* Top Row: Title + Theme Toggle */}
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-3xl font-bold tracking-tight">More Actions</h1>
-        <ThemeToggle />
+        <div onClick={() => setTheme(prev => prev === "dark" ? "light" : "dark")}><ThemeToggle /></div>
       </div>
 
       {/* Promo Card */}
-      <div className="bg-gradient-to-r from-pink-200 via-purple-200 to-indigo-200 rounded-2xl p-4 text-black shadow mb-4">
-        <div className="flex justify-between items-center">
-          <div>
+      <Link to={user.link} className="h-32 w-[100%] group relative flex items-center justify-center overflow-hidden text-primary-foreground shadow rounded-2xl">
+        <div className={`text-primary-foreground absolute left-0 z-[1] h-[100%] w-[200%] bg-gradient-to-r ${theme === "dark" ? "from-pink-200 via-purple-200  to-indigo-300" : "from-purple-400 via-blue-500  to-indigo-700"} px-8 py-4 transition-all duration-400 group-hover:left-[-100%]`}></div>
+        <div className="z-10 flex w-[95%] items-center justify-between">    
+          <div className="flex justify-between items-center">
+            <user.icon className="size-24" />
+            <div>
             <p className="font-bold text-lg">
-              Cashew{" "}
-              <span className="bg-blue-200 text-blue-800 px-2 py-0.5 rounded-full text-sm">
-                Pro
-              </span>
+              {user.name}
             </p>
-            <p className="text-sm mt-1">Budget like a pro with Cashew Pro</p>
+              <p className="text-sm">{user.email}</p>
+              <p className="text-sm">{user.phone}</p>
+            </div>
           </div>
-          <ArrowRight className="w-5 h-5" />
-        </div>
-      </div>
+          <ArrowRight className="w-5 h-5" />   
+        </div> 
+      </Link>
 
       {sections.map((section, i) => (
-        <div key={i} className="space-y-3 mb-8">
+        <div key={i} className="space-y-3 my-8">
            <div>
               <p className="ml-4 text-xl font-bold">{section.name}</p>
               </div>
-          <div className="grid grid-cols-4 gap-3">
+          <div className="flex flex-wrap gap-8">
            
             {section.items.map((item, index) => (
               <Link
                 to={item.link || "#"}
                 key={index}
-                className="bg-background rounded-xl p-4 flex flex-col justify-center items-center space-x-4 hover:bg-secondary border-2 border-solid border-secondary hover:border-muted-foreground transition h-72"
+                className="bg-background rounded-xl p-4 flex flex-col justify-center items-center hover:bg-secondary border-2 border-solid border-secondary-foreground/10 hover:border-muted-foreground transition size-64"
               >
-                <item.icon className="size-32 text-primary" />
-                <div>
-                  <p className="font-medium text-sm">{item.label}</p>
-                  {"subtitle" in item && (
-                    <p className="text-xs text-balance mt-1">{item.subtitle}</p>
-                  )}
+                <item.icon className="size-36 text-primary" />
+                <div className="flex flex-col items-center justify-center h-12">
+                  <p className="font-medium text-sm size-fit">{item.label}</p> 
                 </div>
               </Link>
             ))}
