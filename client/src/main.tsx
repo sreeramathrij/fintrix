4a845714f4c3f6311ff1e39ef5be30d27cf382ee
alwinalbert <160037555+alwinalbert@users.noreply.github.com>
2025-06-09 16:43:39 +0530

    19

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,16 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
-import { I18nextProvider } from "react-i18next" 
 import "./index.css"
-import i18n from "./lib/i18n"
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 <React.StrictMode>
 <BrowserRouter>
- <I18nextProvider i18n={i18n}>
+ 
 <App />
-</I18nextProvider>
+
 </BrowserRouter>
 </React.StrictMode>
 )
